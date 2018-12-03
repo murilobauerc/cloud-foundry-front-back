e72ec57d19845c1e01be3432c2539c8c878f4e84
panchobauer@gmail.com10.96.127.136:133710.96.127.136:133710.96.127.136:13git config --global user.name murilobauerc <panchobauer@gmail.com>
2018-12-03 16:47:50 -0200

    deploying spring project pivotal cloud foundry

---
+++ backend/src/main/java/com/example/cfdemo/CfDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.cfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfDemoApplication.class, args);
+	}
+}
